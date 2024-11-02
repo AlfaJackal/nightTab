75ee0e8b1f95536fb776ee00ec4934eb24fbbfd2
AlfaJackal <github@alfajackal.com>
2024-11-02 01:23:11 +0100

    Update settings-api.js

--- settings-api.js
+++ settings-api.js
@@ -5,7 +5,9 @@ const path = require('path');
 const app = express();
 const PORT = 3100;
 const DATA_PATH = path.join(__dirname, 'data/settings.json');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 // Endpunkt zum Abrufen der Einstellungen
