fd1e2fb71e952a45ce2524a681194886969b3cca
AlfaJackal <github@alfajackal.com>
2024-11-02 01:35:50 +0100

    Test if empty on first start

--- settings-api.js
+++ settings-api.js
@@ -3,12 +3,17 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3100;
+const PORT = 3100; // oder der Port, den Sie festgelegt haben
 const DATA_PATH = path.join(__dirname, 'data/settings.json');
 const cors = require('cors');
-
 app.use(cors());
+
 app.use(express.json());
+
+// Prüfen, ob settings.json existiert und initialisieren, falls nicht
+if (!fs.existsSync(DATA_PATH)) {
+    fs.writeFileSync(DATA_PATH, JSON.stringify({}), 'utf8');
+}
 
 // Endpunkt zum Abrufen der Einstellungen
 app.get('/settings', (req, res) => {
@@ -16,7 +21,7 @@ app.get('/settings', (req, res) => {
         const settings = JSON.parse(fs.readFileSync(DATA_PATH, 'utf8'));
         res.json(settings);
     } else {
-        res.json({}); // Rückgabe eines leeren Objekts, wenn die Datei nicht existiert
+        res.json({}); // Rückgabe leerer Einstellungen, falls keine Datei vorhanden ist
     }
 });
 
